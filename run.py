d4d2fa476d2c81a0523351a99e1812ee59a6be60
semanticdreams <188935636+semanticdreams@users.noreply.github.com>
2025-10-18 10:24:55 +0200

    Simplify run.py

--- run.py
+++ run.py
@@ -3,10 +3,4 @@
 import os
 import subprocess
 
-python_path = os.path.join(
-    os.path.abspath(os.path.dirname(__file__)), '.venv/bin/python')
-
-app_path = os.path.join(
-    os.path.abspath(os.path.dirname(__file__)), 'main.py')
-
-subprocess.call([python_path, app_path])
+subprocess.Popen(['uv', 'run', 'main.py'], cwd=os.path.abspath(os.path.dirname(__file__)))
